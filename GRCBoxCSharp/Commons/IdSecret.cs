3fa1d338cf680dcea702e417c8eaab4c8d30ee7c
sertinell <sertinell@gmail.com>
2015-10-27 15:20:56 +0000

    IdSecret added

---
+++ GRCBoxCSharp/Commons/IdSecret.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRCBoxCSharp
+{   
+    [DataContract]
+    public class IdSecret
+    {
+        [DataMember]
+        public int appId { get; set; }
+        [DataMember]
+        public int secret { get; set; }
+        [DataMember]
+        public long updatePeriod { get; set; }
+    }
+}
