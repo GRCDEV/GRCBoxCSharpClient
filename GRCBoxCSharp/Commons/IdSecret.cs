b2a58170286e7dab89296b9cf78e9e67ade963a3
sertinell <sertinell@gmail.com>
2015-10-30 12:09:07 +0000

    Typo in the namespace fixed

--- GRCBoxCSharp/Commons/IdSecret.cs
+++ GRCBoxCSharp/Commons/IdSecret.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GRCBoxCSharp
+namespace GrcBoxCSharp
 {   
     [DataContract]
     public class IdSecret
